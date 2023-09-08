b73a5023ff6f3bc4b3cafcdcb43b6ca7ed1bfb20
aeropic <38628543+aeropic@users.noreply.github.com>
2023-09-08 13:26:22 +0200

    Add files via upload

--- servVa.lua
+++ servVa.lua
@@ -11,7 +11,10 @@ local function init()
 	i = (100*getValue('VSpd'))    -- should be a float with vertical speed in cm/sec
 	if (i>0) and (i< 30) then i = 0.0 end  -- positive dead zone
 	if (i<0) and (i>-80) then i = 0.0 end  -- negative dead zone
-	i = i*7 -- give some gain to the servo arm
+
+	i = i*5 -- give some gain to the servo arm
+	if (i>0) then i = i + 200 end
+	if (i<0) then i = i -200 end -- spread the positive from the negative Vspeeds
 	serialWrite(i ..'\n')
 end
 
