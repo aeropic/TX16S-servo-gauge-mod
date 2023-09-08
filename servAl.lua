eaa251347f854f2c2fba64e70bba61873571d659
aeropic <38628543+aeropic@users.noreply.github.com>
2023-09-08 14:22:29 +0200

    Add files via upload

--- servAl.lua
+++ servAl.lua
@@ -8,9 +8,9 @@ local function init()
 	-- switch on power on port 0 = aux1 --serialSetPower(port_nr, value)
 	serialSetPower(0, 1)  -- set power on AUX1
 
-	i = (100*getValue('Alt'))    -- should be a float with altitude in meters
+	i = (getValue('Alt'))    -- should be a float with altitude in centimeters
 	if i<0 then i=0.0 end
-	i = 2500 - i*6  -- give some gain to the servo arm : 300 m ==> 1800 usec on the servo ==> ~180 deg
+	i = i*6 - 150*6 -- give some gain to the servo arm : 300 m ==> 1800 usec on the servo ==> ~180 deg, 150m should be horizontal
 	
 	serialWrite(i ..'\n')
 end
