f0a0b322eeae869271214d45d15bef89e357a9b5
aeropic <38628543+aeropic@users.noreply.github.com>
2023-09-11 11:07:01 +0200

    Add files via upload

--- servAl.lua
+++ servAl.lua
@@ -1,15 +1,16 @@
 local function init()
 	-- Called once when the script is loaded
-	  -- switch on power on port 0 = aux1 --serialSetPower(port_nr, value)
-	--serialSetPower(0, 1)
-  end
+	
+end
   
-  local function run()
+local function run()
 	-- switch on power on port 0 = aux1 --serialSetPower(port_nr, value)
 	serialSetPower(0, 1)  -- set power on AUX1
 
-	i = (getValue('Alt'))    -- should be a float with altitude in centimeters
-	if i<0 then i=0.0 end
+	i = getValue('Alt') / 100.0   -- should be a float with altitude in centimeters , convert to meters
+	if  (i < 0) then i = 0.0 end
+	if (i > 600) then i = 0.0 end	-- FSIA6B ibus and edgetx get a bug for negative values of telemetry
+	
 	i = i*6 - 150*6 -- give some gain to the servo arm : 300 m ==> 1800 usec on the servo ==> ~180 deg, 150m should be horizontal
 	
 	serialWrite(i ..'\n')
